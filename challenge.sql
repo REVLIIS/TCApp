e2540b0bb5cfa13eea08ebc630c3bc4279f31c10
Michael <mbroerr9>
2020-12-12 21:28:15 +0100

    newest db

---
+++ challenge.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2020 at 09:26 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `challenge`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_getRecentStatistics` (IN `amount` INT)  NO SQL
+BEGIN
+SELECT * FROM `statistic` ORDER BY date_of_use DESC, time_of_use DESC LIMIT amount;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_insertStatistic` (IN `menu_item_id` INT)  BEGIN
+	insert into statistic (date_of_use, time_of_use, menu_item_id) 
+								VALUES(curdate(), curtime(), menu_item_id);
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `name`) VALUES
+(1, 'Presets'),
+(2, 'Times');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_item`
+--
+
+CREATE TABLE `menu_item` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `count_down` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `menu_item`
+--
+
+INSERT INTO `menu_item` (`id`, `name`, `count_down`, `menu_id`) VALUES
+(1, 'Telefoon', 60, 1),
+(2, 'Etui', 60, 1),
+(3, 'Sleutels', 60, 1),
+(4, '15 Seconden', 15, 2),
+(5, '30 Seconden', 30, 2),
+(6, '45 Seconden', 45, 2),
+(16, 'test!', 55, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statistic`
+--
+
+CREATE TABLE `statistic` (
+  `id` int(11) NOT NULL,
+  `date_of_use` date DEFAULT NULL,
+  `time_of_use` time DEFAULT NULL,
+  `menu_item_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `statistic`
+--
+
+INSERT INTO `statistic` (`id`, `date_of_use`, `time_of_use`, `menu_item_id`) VALUES
+(1, '2020-11-16', '14:50:44', 1),
+(2, '2020-11-16', '14:52:03', 5),
+(3, '2020-11-16', '14:52:42', 4),
+(4, '2020-11-16', '15:01:19', 1),
+(5, '2020-12-12', '14:40:38', 1),
+(6, '2020-12-12', '14:41:05', 1),
+(7, '2020-12-12', '14:47:44', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD UNIQUE KEY `name_UNIQUE` (`name`);
+
+--
+-- Indexes for table `menu_item`
+--
+ALTER TABLE `menu_item`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD UNIQUE KEY `name_UNIQUE` (`name`),
+  ADD KEY `fk_menu_item_menu_idx` (`menu_id`);
+
+--
+-- Indexes for table `statistic`
+--
+ALTER TABLE `statistic`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`id`),
+  ADD KEY `fk_statistic_menu_item1_idx` (`menu_item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `menu_item`
+--
+ALTER TABLE `menu_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `statistic`
+--
+ALTER TABLE `statistic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `menu_item`
+--
+ALTER TABLE `menu_item`
+  ADD CONSTRAINT `fk_menu_item_menu` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`);
+
+--
+-- Constraints for table `statistic`
+--
+ALTER TABLE `statistic`
+  ADD CONSTRAINT `fk_statistic_menu_item1` FOREIGN KEY (`menu_item_id`) REFERENCES `menu_item` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
